2d9f6896bbdad0d2a7ec78958bb9dbd4aa35e616
SuVao <pedro.radon@gmail.com>
2024-05-22 18:16:00 +0100

    1dia

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: pesilva- <pesilva-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:58:00 by pesilva-          #+#    #+#             */
-/*   Updated: 2024/05/22 17:02:30 by pesilva-         ###   ########.fr       */
+/*   Updated: 2024/05/22 18:04:06 by pesilva-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "./mlx_linux/mlx.h"
-#include "./mlx_linux/mlx_int.h"
+#include "minilibx-linux/mlx.h"
+#include <fcntl.h>
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
 
 #endif
--- main1.c
+++ main1.c
@@ -2,10 +2,14 @@
 
 int	main(void)
 {
+	t_data	img;
 	void	*mlx;
 	void	*mlx_win;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
 	mlx_loop(mlx);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, 
+			&img.line_length, &img.endian);
 }
